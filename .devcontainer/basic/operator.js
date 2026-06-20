b8f9afbe55f7db213d4591cb2f97198ff312a796
sagarji <sagarchourasiyajma101@gmail.com>
2026-06-20 08:52:02 +0530

    third day

---
+++ .devcontainer/basic/operator.js
@@ -0,0 +1,12 @@
+//Arithmatics
+let a=10,b=3;
+let sum= a+b;
+console.log("the sum of a and b is "+sum);
+let sub=b-a;
+console.log("the difference of a and b is "+sub);
+let mul = a*b;
+console.log("the product of a and b is "+mul);
+let div =a/b;
+console.log("the division of a and b is "+div);
+let mod =a%b;
+console.log("the modulus of a and b is "+mod);
---
+++ .devcontainer/basic/thirdDay.js
@@ -0,0 +1,11 @@
+let x=10;
+let y=20;
+
+let sum=x+y;
+console.log("the sum of x and y is " +sum)
+
+let name = "sagar";
+let exp=5;
+let isDeveloper=true;
+
+console.log("my name is "+name +" and I have " +exp+"years of experience as a developer");
